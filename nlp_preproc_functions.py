9c754ce593058ea6c0e014fa09ede06f2a3f4336
jon-robbins <117274763+jon-robbins@users.noreply.github.com>
2023-03-28 11:25:55 +0200

    Edits to preproc_functions

--- nlp_preproc_functions.py
+++ nlp_preproc_functions.py
@@ -16,33 +16,16 @@ test_data = pd.DataFrame([
     {'id': 12361624, 'text': 'This is a collection of words'},
 ])
 
-def get_stopwords(df:pd.DataFrame, text_col:str, id_col:str, stopwords=stopwords:set, threshold:float):
-    """
-    This function will take a dataframe and its text, and return the number of documents that contain each individual word. 
-    This is useful for removing stopwords from your documents. A common threshold to use is 0.5 (if a word appears in >=50% of your
-    documents then it's probably not useful for classification).  
-    """
-    word_counts = df.set_index(id_col)[text_col].str.split()\
-        .explode()\
-        .reset_index()\
-        .drop_duplicates()\
-        .groupby('pp_text').size()\
-        .sort_values(ascending=False)
-    word_counts = word_counts.to_dict()
-    word_counts_df = []
-    for i, v in word_counts.items():
-        result = {}
-        result['word'] = i
-        result['count'] = v
-        word_counts_df.append(result)
-    word_counts_df = pd.DataFrame(word_counts_df).sort_values(by='count',ascending=False)
-    word_counts_df['pct'] = word_counts_df['count']/len(df)
-    frequent_words = word_counts_df.loc[word_counts_df['pct'] >= threshold, 'word'].tolist()
-    return word_counts_df, frequent_words
-
-counts, to_add = get_stopwords(test_data, text_col='text', id_col='id', stopwords=stopwords, threshold=0.5)
-
 def preprocess_text(text):
+    """
+    Apply this fucntion to text to:
+    1. lowercase
+    2. remove punctuation
+    3. tokenize
+    4. remove stopwords
+    5. stem
+    6. lemmatize
+    """
     # Convert text to lowercase
     text = text.lower()
 
@@ -68,3 +51,29 @@ def preprocess_text(text):
     preprocessed_text = " ".join(lemmatized_words)
 
     return preprocessed_text
+
+def get_stopwords(df, text_col, id_col, stopwords=stopwords, threshold=0.5):
+    """
+    This function will take a dataframe and its text, and return the number of documents that contain each individual word. 
+    This is useful for removing stopwords from your documents. A common threshold to use is 0.5 (if a word appears in >=50% of your
+    documents then it's probably not useful for classification).  
+
+    This will return two items: the most common words, and a list of words within your given threshold
+    """
+    word_counts = df.set_index(id_col)[text_col].str.split()\
+        .explode()\
+        .reset_index()\
+        .drop_duplicates()\
+        .groupby('pp_text').size()\
+        .sort_values(ascending=False)
+    word_counts = word_counts.to_dict()
+    word_counts_df = []
+    for i, v in word_counts.items():
+        result = {}
+        result['word'] = i
+        result['count'] = v
+        word_counts_df.append(result)
+    word_counts_df = pd.DataFrame(word_counts_df).sort_values(by='count',ascending=False)
+    word_counts_df['pct'] = word_counts_df['count']/len(df)
+    frequent_words = word_counts_df.loc[word_counts_df['pct'] >= threshold, 'word'].tolist()
+    return word_counts_df, frequent_words
