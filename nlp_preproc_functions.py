0a5d006b198c982b71de84176390b806d2a72428
jon-robbins <117274763+jon-robbins@users.noreply.github.com>
2023-03-28 15:17:32 +0200

    Added a couple more preprocessing functions

--- nlp_preproc_functions.py
+++ nlp_preproc_functions.py
@@ -89,3 +89,76 @@ def extract_date(text):
         return pd.to_datetime(date_str, format='%d %B %Y')
     else:
         return np.nan
+    
+def get_eu_legal_type(text):
+    """
+    This will extract the type of legal document it is. Apply this function to the dataframe and create a new column
+    """
+    text = text.lower()
+    
+
+    regex_patterns = [
+        r"(directive)(.*)(of the european parliament and of the council)",
+        r"(decision)(.*)(of the european parliament and of the council)",
+        r"(decision)(.*)(of the european council)",
+        r"(decision)(.*)(of the european council)",
+        r"(regulation)(.*)(of the european parliament and of the council)",
+        r"(decision)(.*)(of the european parliament and of the council)",
+        r"(directive)(.*)(of the european parliament and of the council)",
+        r"(regulation)(.*)(of the council)",
+
+    ]
+
+    for pattern in regex_patterns:
+        match = re.search(pattern, text)
+        if match:
+            return f"{match.group(1).title()} {match.group(3).title()}"
+    
+    legal_types = ["commission regulation", "commission decision", "council regulation",
+                "council directive", "council decision", "commission implementing regulation",
+                "commission delegated regulation", 'decision of the council and the commission', 'political and security committee decision', 'european parliament decision',
+                'decision of the european parliament', 'commission directive', 'decision of the council', 'decision of the european central bank', 'council implementing decision',]
+    regex = r"\b(" + "|".join(legal_types) + r")\b"
+    match = re.search(regex, text)
+    if match:
+        return match.group(1).title()
+    else:
+        return np.nan
+
+def binarize_doc_type(df, doc_type_col='doc_type'):
+    """
+    This should be used in conjunction with the get_eu_legal_type function. First get the title with that function,
+    then use this to match it to the doc. 
+    
+    Documents have different titles indicating the type of document (regulation, decision, directive), and who 
+    published them (parliament, committee, council). This function will add binary features to determine
+    who published it, and what kind of document it is. 
+    """
+    doc_type_map = [
+    {'title': 'Commission Regulation', 'commission': 1, 'regulation': 1},
+    {'title': 'Commission Decision', 'commission': 1, 'decision': 1},
+    {'title': 'Council Regulation', 'council': 1, 'regulation': 1},
+    {'title': 'Council Decision', 'council': 1, 'decision': 1},
+    {'title': 'Directive Of The European Parliament And Of The Council', 'directive': 1, 'parliament': 1, 'council': 1},
+    {'title': 'Council Directive', 'directive': 1, 'council': 1},
+    {'title': 'Regulation Of The European Parliament And Of The Council', 'regulation': 1, 'parliament': 1, 'council': 1},
+    {'title': 'Commission Directive', 'directive': 1, 'commission': 1},
+    {'title': 'Regulation of the Council', 'regulation': 1, 'council': 1},
+    {'title': 'Decision Of The European Parliament And Of The Council', 'decision': 1, 'council': 1, 'parliament': 1},
+    {'title': 'Decision Of The European Parliament', 'decision': 1, 'parliament': 1},
+    {'title': 'Political And Security Committee Decision', 'decision': 1, 'committee': 1},
+    {'title': 'European Parliament Decision', 'decision': 1, 'parliament': 1},
+    {'title': 'Decision Of The Council And The Commission', 'decision': 1, 'council': 1, 'commission': 1},
+    {'title': 'Decision Of The Council', 'decision': 1, 'council': 1},
+    {'title': 'Decision Of The European Council', 'decision': 1, 'council': 1},
+    {'title': 'Council Implementing Decision', 'decision': 1, 'council': 1},
+    {'title': 'Decision Of The European Central Bank', 'decision': 1}
+]
+
+    # Create DataFrame from list of dictionaries
+    doc_types = pd.DataFrame(doc_type_map)
+
+    # Replace missing values with 0
+    doc_types = doc_types.fillna(0)
+    merged_df = pd.merge(df, doc_types, left_on=doc_type_col, right_on='title')
+    return merged_df
